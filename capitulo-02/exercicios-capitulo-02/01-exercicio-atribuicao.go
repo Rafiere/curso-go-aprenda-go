1af25be9e4a76f3b846f506ed93949a6f1f63d9d
Rafael <rafapgbr@gmail.com>
2022-07-28 23:36:22 -0300

    feat: inserção do código da aula sobre atribuição.

---
+++ capitulo-02/exercicios-capitulo-02/01-exercicio-atribuicao.go
@@ -0,0 +1,20 @@
+package exercicios_capitulo_02
+
+import "fmt"
+
+func main() {
+
+	//Atribuindo os valores pedidos no exercício.
+
+	x := 42
+	y := "James Bond"
+	z := true
+
+	//Printando todos os valores com apenas um print.
+	fmt.Println(x, y, z)
+
+	//Printando todos os valores com prints diferentes.
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
