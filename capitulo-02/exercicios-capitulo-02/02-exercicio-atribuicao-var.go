dd1adba01ebe2eb36f5360171a4c95cda2bdd247
Rafael <rafapgbr@gmail.com>
2022-07-28 23:40:45 -0300

    feat: inserção do código da aula sobre declaração de variáveis com escopo de pacote.

---
+++ capitulo-02/exercicios-capitulo-02/02-exercicio-atribuicao-var.go
@@ -0,0 +1,27 @@
+package exercicios_capitulo_02
+
+import (
+	"fmt"
+)
+
+var x int
+var y string
+var z bool //Atribuindo os tipos para as variáveis com escopo de pacote.
+
+//Outras variações na forma de declarar variáveis.
+var (
+	a int
+	b string
+	c bool
+)
+
+var (
+	d int
+	e string
+	f bool
+)
+
+func main() {
+
+	fmt.Println()
+}
